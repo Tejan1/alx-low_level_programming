9f1e43f6885845f50680f825afa3a991e912f94a
tejan kamara <najetkam1@gmail.com>
2023-02-16 16:45:32 -0800

    task 5 corrections

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,9 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- * Return: Always return 0 (Success)
- */
-
-int main(void)
-{
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-}
+#include <stdio.h>                                                                  
+/**                                                                                 
+ *  * Description: main - Prints out a string to stdout.                            
+ *   * Return: 0 if success.                                                        
+*/                                                                                  
+int main(void)                                                                      
+{                                                                                   
+printf("with proper grammar, but the outcome is a piece of art,\n");                
+return (0);}
